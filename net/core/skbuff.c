770340ac331d4bd4ca828756ca77dd33a3d5145b
Boris Pismenny <borisp@nvidia.com>
2020-05-18 19:40:44 +0300

    net: Introduce direct data placement tcp offload
    This commit introduces direct data placement (DDP) offload for TCP.
    
    The motivation is saving compute resources/cycles that are spent
    to copy data from SKBs to the block layer buffers and CRC
    calculation/verification for received PDUs (Protocol Data Units).
    
    The DDP capability is accompanied by new net_device operations that
    configure hardware contexts.
    
    There is a context per socket, and a context per DDP operation.
    Additionally, a resynchronization routine is used to assist
    hardware handle TCP OOO, and continue the offload. Furthermore,
    we let the offloading driver advertise what is the max hw
    sectors/segments.
    
    The interface includes the following net-device ddp operations:
    
     1. sk_add - add offload for the queue represented by socket+config pair
     2. sk_del - remove the offload for the socket/queue
     3. ddp_setup - request copy offload for buffers associated with an IO
     4. ddp_teardown - release offload resources for that IO
     5. limits - query NIC driver for quirks and limitations (e.g.
                 max number of scatter gather entries per IO)
     6. set_caps - request ULP DDP capabilities enablement
     7. get_caps - request current ULP DDP capabilities
     8. get_stats - query NIC driver for ULP DDP stats
    
    Using this interface, the NIC hardware will scatter TCP payload
    directly to the BIO pages according to the command_id.
    
    To maintain the correctness of the network stack, the driver is
    expected to construct SKBs that point to the BIO pages.
    
    The SKB passed to the network stack from the driver represents
    data as it is on the wire, while it is pointing directly to data
    in destination buffers.
    
    As a result, data from page frags should not be copied out to
    the linear part. To avoid needless copies, such as when using
    skb_condense, we mark the skb->no_condense bit.
    In addition, the skb->ulp_crc will be used by the upper layers to
    determine if CRC re-calculation is required. The two separated skb
    indications are needed to avoid false positives GRO flushing events.
    
    Follow-up patches will use this interface for DDP in NVMe-TCP.
    
    Capability bits stored in net_device allow drivers to report which
    ULP DDP capabilities a device supports. Control over these
    capabilities will be exposed to userspace in later patches.
    
    Issue: 2151469
    Change-Id: If5b05a50918241d3a49f92ac8699a7d400e47a26
    Signed-off-by: Boris Pismenny <borisp@nvidia.com>
    Signed-off-by: Ben Ben-Ishay <benishay@nvidia.com>
    Signed-off-by: Or Gerlitz <ogerlitz@nvidia.com>
    Signed-off-by: Yoray Zack <yorayz@nvidia.com>
    Signed-off-by: Shai Malin <smalin@nvidia.com>
    Signed-off-by: Aurelien Aptel <aaptel@nvidia.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -77,6 +77,7 @@
 #include <net/mctp.h>
 #include <net/page_pool/helpers.h>
 #include <net/dropreason.h>
+#include <net/ulp_ddp.h>
 
 #include <linux/uaccess.h>
 #include <trace/events/skb.h>
@@ -6752,7 +6753,7 @@ void skb_condense(struct sk_buff *skb)
 {
 	if (skb->data_len) {
 		if (skb->data_len > skb->end - skb->tail ||
-		    skb_cloned(skb))
+		    skb_cloned(skb) || skb_is_no_condense(skb))
 			return;
 
 		/* Nice, we can free page frag(s) right now */
