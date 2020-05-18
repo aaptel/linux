25f0abc819f80a94431c335edf917eccddc47235
Boris Pismenny <borisp@nvidia.com>
2020-05-18 19:40:44 +0300

    net: Introduce direct data placement tcp offload
    This commit introduces direct data placement (DDP) offload for TCP.
    
    The motivation is saving compute resources/cycles that are spent
    to copy data from SKBs to the block layer buffers and CRC
    calculation/verification for received PDUs (Protocol Data Units).
    
    The DDP capability is accompanied by new net_device operations that
    configure hardware contexts.
    
    There is a context per socket, and a context per DDP operation.
    Additionally, a resynchronization routine is used to assist
    hardware handle TCP OOO, and continue the offload. Furthermore,
    we let the offloading driver advertise what is the max hw
    sectors/segments.
    
    The interface includes the following net-device ddp operations:
    
     1. sk_add - add offload for the queue represented by socket+config pair
     2. sk_del - remove the offload for the socket/queue
     3. ddp_setup - request copy offload for buffers associated with an IO
     4. ddp_teardown - release offload resources for that IO
     5. limits - query NIC driver for quirks and limitations (e.g.
                 max number of scatter gather entries per IO)
     6. set_caps - request ULP DDP capabilities enablement
     7. get_stats - query NIC driver for ULP DDP stats
    
    Using this interface, the NIC hardware will scatter TCP payload
    directly to the BIO pages according to the command_id.
    
    To maintain the correctness of the network stack, the driver is
    expected to construct SKBs that point to the BIO pages.
    
    The SKB passed to the network stack from the driver represents
    data as it is on the wire, while it is pointing directly to data
    in destination buffers.
    
    As a result, data from page frags should not be copied out to
    the linear part. To avoid needless copies, such as when using
    skb_condense, we mark the skb->ulp_ddp bit.
    In addition, the skb->ulp_crc will be used by the upper layers to
    determine if CRC re-calculation is required. The two separated skb
    indications are needed to avoid false positives GRO flushing events.
    
    Follow-up patches will use this interface for DDP in NVMe-TCP.
    
    Capability bits stored in net_device allow drivers to report which
    ULP DDP capabilities a device supports. Control over these
    capabilities will be exposed to userspace in later patches.
    
    Signed-off-by: Boris Pismenny <borisp@nvidia.com>
    Signed-off-by: Ben Ben-Ishay <benishay@nvidia.com>
    Signed-off-by: Or Gerlitz <ogerlitz@nvidia.com>
    Signed-off-by: Yoray Zack <yorayz@nvidia.com>
    Signed-off-by: Shai Malin <smalin@nvidia.com>
    Signed-off-by: Aurelien Aptel <aaptel@nvidia.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -810,6 +810,8 @@ typedef unsigned char *sk_buff_data_t;
  *		delivery_time in mono clock base (i.e. EDT).  Otherwise, the
  *		skb->tstamp has the (rcv) timestamp at ingress and
  *		delivery_time at egress.
+ *	@ulp_ddp: DDP offloaded
+ *	@ulp_crc: CRC offloaded
  *	@napi_id: id of the NAPI struct this skb came from
  *	@sender_cpu: (aka @napi_id) source CPU in XPS
  *	@alloc_cpu: CPU which did the skb allocation.
@@ -989,7 +991,10 @@ struct sk_buff {
 #if IS_ENABLED(CONFIG_IP_SCTP)
 	__u8			csum_not_inet:1;
 #endif
-
+#ifdef CONFIG_ULP_DDP
+	__u8                    ulp_ddp:1;
+	__u8			ulp_crc:1;
+#endif
 #if defined(CONFIG_NET_SCHED) || defined(CONFIG_NET_XGRESS)
 	__u16			tc_index;	/* traffic control index */
 #endif
@@ -5062,6 +5067,24 @@ static inline void skb_mark_for_recycle(struct sk_buff *skb)
 
 ssize_t skb_splice_from_iter(struct sk_buff *skb, struct iov_iter *iter,
 			     ssize_t maxsize, gfp_t gfp);
+
+static inline bool skb_is_ulp_ddp(struct sk_buff *skb)
+{
+#ifdef CONFIG_ULP_DDP
+	return skb->ulp_ddp;
+#else
+	return 0;
+#endif
+}
+
+static inline bool skb_is_ulp_crc(struct sk_buff *skb)
+{
+#ifdef CONFIG_ULP_DDP
+	return skb->ulp_crc;
+#else
+	return 0;
+#endif
+}
 
 #endif	/* __KERNEL__ */
 #endif	/* _LINUX_SKBUFF_H */
