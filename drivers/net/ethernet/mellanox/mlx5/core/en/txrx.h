7236096ec2334fd9b163eb6efaea6bcbd402d632
Ben Ben-Ishay <benishay@nvidia.com>
2020-11-01 19:29:46 +0200

    net/mlx5e: NVMEoTCP, queue init/teardown
    Adds the ddp ops of sk_add, sk_del and offload limits.
    
    When nvme-tcp establishes new queue/connection, the sk_add op is called.
    We allocate a hardware context to offload operations for this queue:
    - use a steering rule based on the connection 5-tuple to mark packets
      of this queue/connection with a flow-tag in their completion (cqe)
    - use a dedicated TIR to identify the queue and maintain the HW context
    - use a dedicated ICOSQ to maintain the HW context by UMR postings
    - use a dedicated tag buffer for buffer registration
    - maintain static and progress HW contexts by posting the proper WQEs.
    
    When nvme-tcp teardowns a queue/connection, the sk_del op is called.
    We teardown the queue and free the corresponding contexts.
    
    The offload limits we advertise deal with the max SG supported.
    
    [Re-enabled calling open/close icosq out of en_main.c]
    
    Signed-off-by: Ben Ben-Ishay <benishay@nvidia.com>
    Signed-off-by: Boris Pismenny <borisp@nvidia.com>
    Signed-off-by: Or Gerlitz <ogerlitz@nvidia.com>
    Signed-off-by: Yoray Zack <yorayz@nvidia.com>
    Signed-off-by: Aurelien Aptel <aaptel@nvidia.com>
    Reviewed-by: Tariq Toukan <tariqt@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en/txrx.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en/txrx.h
@@ -73,6 +73,7 @@ enum mlx5e_icosq_wqe_type {
 #endif
 #ifdef CONFIG_MLX5_EN_NVMEOTCP
 	MLX5E_ICOSQ_WQE_UMR_NVMEOTCP,
+	MLX5E_ICOSQ_WQE_SET_PSV_NVMEOTCP,
 #endif
 };
 
@@ -258,6 +259,11 @@ struct mlx5e_icosq_wqe_info {
 		struct {
 			struct mlx5e_ktls_rx_resync_buf *buf;
 		} tls_get_params;
+#endif
+#ifdef CONFIG_MLX5_EN_NVMEOTCP
+		struct {
+			struct mlx5e_nvmeotcp_queue *queue;
+		} nvmeotcp_q;
 #endif
 	};
 };
