eca23b3fa036f2a76e463a3ed26ff9dad4717858
Aurelien Aptel <aaptel@nvidia.com>
2022-08-10 16:46:03 +0300

    net/mlx5e: NVMEoTCP, statistics
    NVMEoTCP offload statistics include both control and data path
    statistic: counters for the netdev ddp ops, offloaded packets/bytes,
    resync and dropped packets.
    
    Expose the statistics using ulp_ddp_ops->get_stats()
    instead of the regular statistics flow.
    
    Signed-off-by: Ben Ben-Ishay <benishay@nvidia.com>
    Signed-off-by: Boris Pismenny <borisp@nvidia.com>
    Signed-off-by: Or Gerlitz <ogerlitz@nvidia.com>
    Signed-off-by: Yoray Zack <yorayz@nvidia.com>
    Signed-off-by: Shai Malin <smalin@nvidia.com>
    Signed-off-by: Aurelien Aptel <aaptel@nvidia.com>
    Reviewed-by: Tariq Toukan <tariqt@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_stats.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en_stats.h
@@ -132,6 +132,9 @@ void mlx5e_stats_ts_get(struct mlx5e_priv *priv,
 			struct ethtool_ts_stats *ts_stats);
 void mlx5e_get_link_ext_stats(struct net_device *dev,
 			      struct ethtool_link_ext_stats *stats);
+struct ulp_ddp_stats;
+void mlx5e_stats_ulp_ddp_get(struct mlx5e_priv *priv,
+			     struct ulp_ddp_stats *stats);
 
 /* Concrete NIC Stats */
 
@@ -405,6 +408,12 @@ struct mlx5e_rq_stats {
 	u64 tls_resync_res_retry;
 	u64 tls_resync_res_skip;
 	u64 tls_err;
+#endif
+#ifdef CONFIG_MLX5_EN_NVMEOTCP
+	u64 nvmeotcp_drop;
+	u64 nvmeotcp_resync;
+	u64 nvmeotcp_packets;
+	u64 nvmeotcp_bytes;
 #endif
 };
 
