b5e7b1f017bc72871a436c733ea0e0e191529ec5
Aurelien Aptel <aaptel@nvidia.com>
2022-11-24 13:04:28 +0200

    net/ethtool: add new stringset ETH_SS_ULP_DDP_{CAPS,STATS}
    This commit exposes ULP DDP capability and statistics names to
    userspace via netlink.
    
    In order to support future ULP DDP capabilities and statistics without
    having to change the netlink protocol (and userspace ethtool) we add
    new string sets to let userspace dynamically fetch what the kernel
    supports.
    
    * ETH_SS_ULP_DDP_CAPS stores names of ULP DDP capabilities
    * ETH_SS_ULP_DDP_STATS stores names of ULP DDP statistics.
    
    These stringsets will be used in later commits when implementing the
    new ULP DDP GET/SET netlink messages.
    
    We keep the convention of strset.c of having the static_assert()
    right after the array declaration, despite the checkpatch warning.
    
    Signed-off-by: Shai Malin <smalin@nvidia.com>
    Signed-off-by: Aurelien Aptel <aaptel@nvidia.com>

--- include/uapi/linux/ethtool.h
+++ include/uapi/linux/ethtool.h
@@ -681,6 +681,8 @@ enum ethtool_link_ext_substate_module {
  * @ETH_SS_STATS_ETH_MAC: names of IEEE 802.3 MAC statistics
  * @ETH_SS_STATS_ETH_CTRL: names of IEEE 802.3 MAC Control statistics
  * @ETH_SS_STATS_RMON: names of RMON statistics
+ * @ETH_SS_ULP_DDP_CAPS: names of ULP DDP capabilities
+ * @ETH_SS_ULP_DDP_STATS: names of ULP DDP statistics
  *
  * @ETH_SS_COUNT: number of defined string sets
  */
@@ -706,6 +708,8 @@ enum ethtool_stringset {
 	ETH_SS_STATS_ETH_MAC,
 	ETH_SS_STATS_ETH_CTRL,
 	ETH_SS_STATS_RMON,
+	ETH_SS_ULP_DDP_CAPS,
+	ETH_SS_ULP_DDP_STATS,
 
 	/* add new constants above here */
 	ETH_SS_COUNT
